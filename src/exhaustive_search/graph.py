daf3947282c480bd0f1d2afab11c9bb1153bcd71
Ciaran Downey <me@ciarand.me>
2015-04-19 19:28:13 -0700

    Fix weight() invocation, add tests

--- src/exhaustive_search/graph.py
+++ src/exhaustive_search/graph.py
@@ -1,21 +1,39 @@
 """ Graph is provided to add some structure to the algorithms """
 
+import math
+
 class Edge:
     def __init__(self, left, right):
         if left.x == right.x and left.y == right.y:
             raise ValueError("x and y cannot be the same point")
 
+        # deterministically order so __eq__ and __hash__ checks are easy
+        if (left.x < right.x) or (left.x == right.x and left.y < right.y):
+            left, right = right, left
+
         self.left = left
         self.right = right
 
     def weight(self):
-        return self.left.y - self.right.y
+        return math.sqrt(((self.left.y - self.right.y) ** 2) + ((self.left.x - self.right.x) ** 2))
+
+    def __eq__(self, other):
+        return self.left == other.left and self.right == other.right
+
+    def __hash__(self):
+        return hash(''.join(str(v) for v in [self.left.x, self.left.y]))
 
 class Graph:
     def __init__(self, points):
-        edges = []
+        self.edges = set([])
 
         for origin in points:
             for dest in points:
-                edges.append(Edge(origin, dest))
+                if dest != origin:
+                    self.edges.add(Edge(origin, dest))
 
+    def num_edges(self):
+        return len(self.edges)
+
+    def __iter__(self):
+        return iter(self.edges)
--- test/test_edge_weight_implementation.py
+++ test/test_edge_weight_implementation.py
@@ -13,6 +13,24 @@ def test_raises_exception_when_created_with_duplicate_point():
 
 def test_correctly_calculates_the_weight_of_a_straight_edge():
     """ weight() should be able to handle a straight distance calculation """
-    e = Edge(Point(0, 3), Point(0, 0))
+    assert Edge(Point(0, 3), Point(0, 0)).weight() == 3, __doc__
+    assert Edge(Point(3, 0), Point(0, 0)).weight() == 3, __doc__
 
-    assert e.weight() == 3, __doc__
+def test_probably_correctly_compares_two_weights():
+    """ weight() should be able to correctly compare two simple dissimilar points"""
+    smaller = Edge(Point(0, 3), Point(0, 0)).weight()
+    larger = Edge(Point(0, 3), Point(5, 0)).weight()
+
+    assert larger > smaller, __doc__
+    assert larger >= smaller, __doc__
+
+    assert not larger < smaller, __doc__
+    assert not larger <= smaller, __doc__
+
+    assert larger != smaller, __doc__
+
+    assert smaller < larger, __doc__
+    assert smaller <= larger, __doc__
+
+    assert not smaller > larger, __doc__
+    assert not smaller >= larger, __doc__
