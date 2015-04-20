6c06a7b4fa4570c8a88b8944a1455bb145520a0d
Ciaran Downey <me@ciarand.me>
2015-04-19 17:49:27 -0700

    Move Point to a separate (in-package) class

---
+++ src/exhaustive_search/point.py
@@ -0,0 +1,9 @@
+""" Point is copied directly from the provided stubcode """
+
+class Point:
+    """ Represents a single 2D point. """
+
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
